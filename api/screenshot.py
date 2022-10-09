a7b19bd6bb6956e7a936b61da5fe1cbf15f35911
applemango <apple.mangoooo97@gmail.com>
2022-10-09 10:12:35 +0900

    add target element

--- api/screenshot.py
+++ api/screenshot.py
@@ -3,23 +3,43 @@ from time import sleep
 from werkzeug.utils import secure_filename
 from utils import parse_url
 from os import path
+import logging
 
 def init_screenshot():
-    global driver
+    global driver, logger
+
+    logger = logging.getLogger('urllib3.connectionpool')
+    logger.setLevel(logging.INFO)
+    logger = logging.getLogger('selenium.webdriver.remote.remote_connection')
+    logger.setLevel(logging.WARNING)
+
     options = webdriver.ChromeOptions()
-    options.add_argument('headless')
+    #options.add_argument('headless')
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-extensions')
+    options.add_argument("--disable-gpu");
+    options.add_argument("--disable-crash-reporter");
+    options.add_argument("--disable-in-process-stack-traces");
+    options.add_argument("--disable-logging");
+    options.add_argument("--disable-dev-shm-usage");
+    options.add_argument("--log-level=3");
+    options.add_argument("--output=/dev/null");
     driver = webdriver.Chrome(executable_path="./chromedriver.exe", chrome_options=options)
 
 def close_screenshot():
     driver.quit()
 
 def screenshot(url: str)-> str:
+    print(url, parse_url(url))
     url = parse_url(url)
     filename = secure_filename("{}.png".format(url))
     if path.exists("./tmp/{}".format(filename)):
         return filename
+    #driver.implicitly_wait(1)
     driver.get(url)
+    #driver.implicitly_wait(2)
     driver.execute_script('var x = document.createElement("style");x.innerHTML = "*{overflow:hidden !important}";document.querySelector("body").append(x)')
     driver.get_screenshot_as_file("./tmp/{}".format(filename))
-    sleep(1)
     return filename
--- extension/src/main.js
+++ extension/src/main.js
@@ -1,8 +1,15 @@
-const elements = document.querySelectorAll("#rso .MjjYud > div.g.Ww4FFb.vt6azd.tF2Cxc")
+const elements = document.querySelectorAll("#rso .MjjYud > div.g.Ww4FFb.vt6azd.tF2Cxc, .MjjYud .g .BYM4Nd .eKjLze .g, .CCcQAA .g > div > div, .hlcw0c > .MjjYud .g > div:not(.kvH3mc) > div:not(.Z26q7c)")
 elements.forEach(function(element, i){
-    const url = element.childNodes[0].childNodes[0].childNodes[0].childNodes[0].href
+    let url;
+    try {
+        url = element.childNodes[0].childNodes[0].childNodes[0].childNodes[0].href
+    } catch (e) {
+        if( e instanceof TypeError) {
+            url = element.childNodes[1].childNodes[0].childNodes[0].childNodes[0].href
+        }
+    }
     const e = `
-    <a href="${url}" style="width:111px;height:82px">
+    <a href="${url}" style="width:111px;display:flex;/*align-items: center*/;justify-content: center;">
         <img loading="lazy" align="left" src="http://127.0.0.1:4200/get/?url=${url}" style="width:111px;height:82px">
     </a>`
     element.style.display = "flex"
